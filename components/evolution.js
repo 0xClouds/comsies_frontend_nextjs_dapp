cd548c0a37a37239f48d2d15ac8081db9ebc4f11
Cloud <cloud.frontier0@gmail.com>
2022-09-30 10:17:34 -0400

    made evolution page random

--- components/evolution.js
+++ components/evolution.js
@@ -3,17 +3,18 @@ import styles from "../styles/Evolution.module.scss";
 import { images } from "./images";
 
 export default function Evolution() {
-  // const randomIndex = Math.floor(Math.random() * 9);
+  
+  const randomIndex = Math.floor(Math.random() * 9);
   return (
     <div className={styles.jambis}>
       <div className={`${styles.jambi} ${styles.first}`}>
-        <Image src={images[0].v1} width="400" height="400" alt={images[0].name}></Image>
+        <Image src={images[randomIndex].v1} width="400" height="400" alt={images[0].name}></Image>
       </div>
       <div className={`${styles.jambi} ${styles.second}`}>
-        <Image src={images[0].v2} width="400" height="400" alt={images[0].name}></Image>
+        <Image src={images[randomIndex].v2} width="400" height="400" alt={images[0].name}></Image>
       </div>
       <div className={`${styles.jambi} ${styles.third}`}>
-        <Image src={images[0].v3} alt={images[0].name}></Image>
+        <Image src={images[randomIndex].v3} alt={images[0].name}></Image>
       </div>
     </div>
   );
